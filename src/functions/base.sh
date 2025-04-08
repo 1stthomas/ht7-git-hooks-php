613940f917b04c1019c89df10654b5f3d555d7e1
1stthomas <thomas.pluess@students.ffhs.ch>
2025-04-08 23:15:45 +0200

    #1 add base command check which has to be improved when the related hooks are programmed

--- src/functions/base.sh
+++ src/functions/base.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+check_commands() {
+    local commands=("php" "phpstan" "php-cs-fixer" "phpunit")
+    for cmd in "${commands[@]}"; do
+        if ! command -v "$cmd" &> /dev/null; then
+            echo "$cmd could not be found. Please install it."
+            # exit 1
+        fi
+    done
+}
+
 get_comment_intro() {
     local source=${2:-"CLI"}
     local task=${1:-"misc"}
