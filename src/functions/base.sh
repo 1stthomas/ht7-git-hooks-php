31d255879b77471ca12954f7403dbeae22efc2e1
1stthomas <thomas.pluess@students.ffhs.ch>
2025-04-09 22:48:55 +0200

    #1 add phpstan hook

--- src/functions/base.sh
+++ src/functions/base.sh
@@ -22,13 +22,13 @@ print_intro() {
     cat << "EOF"
 
 =========================================================================================
- 
   _     _  _____           _ _        _                 _                    _           
  | |__ | ||___  |     __ _(_) |_     | |__   ___   ___ | | _____       _ __ | |__  _ __  
  | '_ \| __| / /____ / _` | | __|____| '_ \ / _ \ / _ \| |/ / __|_____| '_ \| '_ \| '_ \ 
  | | | | |_ / /_____| (_| | | ||_____| | | | (_) | (_) |   <\__ \_____| |_) | | | | |_) |
  |_| |_|\__/_/       \__, |_|\__|    |_| |_|\___/ \___/|_|\_\___/     | .__/|_| |_| .__/ 
                      |___/                                            |_|         |_|    
+
 EOF
 }
 
@@ -50,19 +50,18 @@ process_selection() {
     local selection=$1
     case $selection in
         1)
-            echo -e "\n=====Syntax Check====="
+            echo -e "\n=====Syntax Check - Not implemented ====="
             # php -l src/functions/base.sh
             ;;
         2)
-            echo -e "\n=====PHPStan====="
-            # phpstan analyse src/functions/base.sh
+            source "$(pwd)/hooks/phpstan.sh"
             ;;
         3)
-            echo -e "\n=====PHP CS Fixer====="
+            echo -e "\n=====PHP CS Fixer - Not implemented ====="
             # php-cs-fixer fix src/functions/base.sh
             ;;
         4)
-            echo -e "\n=====PHPUnit====="
+            echo -e "\n=====PHPUnit - Not implemented ====="
             # phpunit tests/
             ;;
         *)
